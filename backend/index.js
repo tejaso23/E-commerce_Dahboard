0046cfbf510e44d8760f9c0bea4df7935156a051
tejaso23 <80096396+tejaso23@users.noreply.github.com>
2024-08-18 00:14:28 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 const jwtkey = process.env.JWT_SECRET || "e-comm";
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:9000';
 
 export default API_URL;
